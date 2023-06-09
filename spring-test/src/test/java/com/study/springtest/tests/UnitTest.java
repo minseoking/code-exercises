4bda295dabc00a56a821e7f611a23797f955121c
minseoking <lms98923@naver.com>
2023-06-09 11:24:44 +0900

    단위 테스트 샘플 추가

---
+++ spring-test/src/test/java/com/study/springtest/tests/UnitTest.java
@@ -0,0 +1,66 @@
+package com.study.springtest.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UnitTest {
+
+    @Test
+    @DisplayName("메뉴 변경 테스트")
+    void changeMenuTest() {
+        Menu menu = new Menu("국밥");
+        menu.changeMenu("냉면");
+
+        assertThat(menu.getName()).isEqualTo("냉면");
+    }
+
+    @Test
+    @DisplayName("주문 테스트")
+    void orderTest() {
+        Menu menu1 = new Menu("국밥");
+        Menu menu2 = new Menu("냉면");
+
+        Order order = new Order(List.of(menu1, menu2));
+
+        order.addMenu(new Menu("돈까스"));
+
+        assertThat(order.getMenus().size()).isEqualTo(3);
+    }
+
+    private class Menu {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public Menu(String name) {
+            this.name = name;
+        }
+
+        public void changeMenu(String name) {
+            this.name = name;
+        }
+    }
+
+    private class Order {
+        private List<Menu> menus = new ArrayList<>();
+
+        public List<Menu> getMenus() {
+            return menus;
+        }
+
+        public void addMenu(Menu menu) {
+            menus.add(menu);
+        }
+        public Order(List<Menu> menus) {
+            this.menus.addAll(menus);
+        }
+    }
+}
+
