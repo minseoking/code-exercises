ee0bfc1e122556c168bf01e301d7f415e6c68de2
minseoking <lms98923@naver.com>
2023-08-08 13:51:27 +0900

    스프링 MSA 프로젝트 추가

---
+++ spring-msa/README.md
@@ -0,0 +1,4 @@
+# 스프링 MSA
+
+[MSA 공작소 개정 2](https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=296893396)
+책을 읽으면서 공부한 내용을 정리한 저장소입니다.
---
+++ spring-msa/src/test/java/com/example/springmsa/SpringMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
