90aad1bc7f683a7ec5810dbba91f3a14266cea51
minseoking <lms98923@naver.com>
2023-08-10 18:00:56 +0900

    스프링 MSA 프로젝트 국제화 추가 중

--- spring-msa/src/main/java/com/example/springmsa/SpringMsaApplication.java
+++ spring-msa/src/main/java/com/example/springmsa/SpringMsaApplication.java
@@ -2,6 +2,12 @@ package com.example.springmsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringMsaApplication {
@@ -10,4 +16,18 @@ public class SpringMsaApplication {
         SpringApplication.run(SpringMsaApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("message");
+        source.setUseCodeAsDefaultMessage(true);
+        return source;
+    }
 }
